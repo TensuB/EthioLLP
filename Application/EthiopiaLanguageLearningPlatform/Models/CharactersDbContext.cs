7ab64909e4a285516137b831c49369f736c069c6
tensay bulcha <tensay.bulcha@gmail.com>
2020-05-21 13:02:06 +0100

    Business Models added

---
+++ Application/EthiopiaLanguageLearningPlatform/Models/CharactersDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace EthiopiaLanguageLearningPlatform.Models
+{
+    public class CharactersDbContext : DbContext
+    {
+
+        public CharactersDbContext(DbContextOptions<CharactersDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Character> Characters { get; set; }
+    }
+}
