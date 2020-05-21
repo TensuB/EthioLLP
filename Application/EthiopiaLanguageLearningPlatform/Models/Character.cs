af5c77f19bf7c87c41805d91f78f796f9d0ff61f
tensay bulcha <tensay.bulcha@gmail.com>
2020-05-21 20:52:16 +0100

    First Amharic letter retrieved from db

--- Application/EthiopiaLanguageLearningPlatform/Models/Character.cs
+++ Application/EthiopiaLanguageLearningPlatform/Models/Character.cs
@@ -2,7 +2,7 @@
 {
     public class Character
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string Name {get; set;}
         public string Symbol { get; set; }
         public string ImageUrl { get; set; }
