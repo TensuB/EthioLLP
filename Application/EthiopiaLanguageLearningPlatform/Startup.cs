71ad30a0a83b19977b3c7419cd10a9d652731945
tensay bulcha <tensay.bulcha@gmail.com>
2020-05-29 13:46:16 +0100

    locally working versions test deployment

--- Application/EthiopiaLanguageLearningPlatform/Startup.cs
+++ Application/EthiopiaLanguageLearningPlatform/Startup.cs
@@ -19,7 +19,7 @@ namespace EthiopiaLanguageLearningPlatform
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews();    
 
             var connectionString = Configuration.GetConnectionString("CharactersDbContext");
 
